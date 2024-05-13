4e8e28905429290eb833b7d940bde4b08cd94af3
@colecmc <colecmc@correctlydesigned.net>
2024-05-13 08:29:25 -0500

    chore: add semicolons
    Add missing semicolons and do not rely ASI.
    
    ---

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from "express"
-import { data } from "./static.json.mjs"
+import express from "express";
+import { data } from "./static.json.mjs";
 
 const server = express();
 
 const port = 8090;
 
 server.all('/', (req, res) => {
-    res.json(data);
+  res.json(data);
 });
 
 server.listen(port, () => {
